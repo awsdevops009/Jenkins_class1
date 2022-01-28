d6b3d3ad66a2dc216f86ceb619b567a7cba8cce4
awsdevops009 <47270229+awsdevops009@users.noreply.github.com>
2022-01-28 17:43:53 +0530

    Update makeindex.go

--- src/makeindex.go
+++ src/makeindex.go
@@ -20,7 +20,7 @@ func main() {
 <h1>The curent server time is 
 <%
          Date date = new Date();
-         out.print( "<h2 align = \"center\">" +date.toString()+"</h2>");
+         out.print( "<h4 align = \"center\">" +date.toString()+"</h4>");
  %>
 </h1>
 </body>
