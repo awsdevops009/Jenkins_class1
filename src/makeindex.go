355ce2ae829937c603bddf3566f2be166b4f8d0d
awsdevops009 <47270229+awsdevops009@users.noreply.github.com>
2024-06-09 10:10:08 +0530

    Update makeindex.go

--- src/makeindex.go
+++ src/makeindex.go
@@ -12,35 +12,32 @@ func main() {
 		log.Fatal("Cannot create file", err)
 	}
 	defer file.Close()
+
 	v := "docker"
 	s := `<html>
 <script>
 function startTime() {
-var today = new Date();
-var h = today.getHours();
-var m = today.getMinutes();
-var s = today.getSeconds();
-m = checkTime(m);
-s = checkTime(s);
-document.getElementById('clock').innerHTML =
-h + ":" + m + ":" + s;
-var t = setTimeout(startTime, 500);
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
-if (i < 10) {i = "0" + i};  
-return i;
+    if (i < 10) {i = "0" + i};  
+    return i;
 }
 </script>
-	<body onload="startTime()">
-	<h2>Hello from  ` + v + ` !</h2>
-
-	<h1>The curent server time is </h1>
-	<div id="clock"></div>
-	
-	</body>
-
-
+<body onload="startTime()">
+    <h2>Hello from ` + v + ` !</h2>
+    <h1>The current server time is</h1>
+    <div id="clock"></div>
 </body>
 </html>`
+
 	fmt.Fprintf(file, s)
 }
