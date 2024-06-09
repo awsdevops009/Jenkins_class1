112a243196cf15938b41747454f8b94dacf8b12c
awsdevops009 <47270229+awsdevops009@users.noreply.github.com>
2024-06-09 10:41:17 +0530

    Update makeindex.go

--- src/makeindex.go
+++ src/makeindex.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -18,15 +19,22 @@ func main() {
 		v = "docker"
 	}
 
-	s := `<html>
+	// Get the current date
+	currentDate := time.Now().Local()
+
+	// Get the day of the week (e.g., Monday, Tuesday, etc.)
+	dayOfWeek := currentDate.Weekday().String()
+
+	s := fmt.Sprintf(`<html>
 <head>
 <title>Welcome to Our Website</title>
 </head>
 <body>
-<h2>Hello ` + v + ` the Tomcat server is running!</h2>
-<h1>The current server time is ${java.time.LocalDateTime.now()}</h1>
+<h2>Hello %s the Tomcat server is running!</h2>
+<h1>The current server time is %s</h1>
+<h1>Today's day is %s</h1>
 </body>
-</html>`
+</html>`, v, currentDate.Format("2006-01-02 15:04:05"), dayOfWeek)
 
 	fmt.Fprintln(file, s)
 }
