38ff39bc73123bd99e558571a1ad724a88cfc036
awsdevops009 <47270229+awsdevops009@users.noreply.github.com>
2024-06-09 10:26:39 +0530

    Update makeindex.go

--- src/makeindex.go
+++ src/makeindex.go
@@ -12,20 +12,21 @@ func main() {
 		log.Fatal("Cannot create file", err)
 	}
 	defer file.Close()
+
 	v := os.Getenv("name")
 	if v == "" {
 		v = "docker"
 	}
-	s := `<%@ page import="java.io.*,java.util.*,javax.servlet.*" %>
-<html>
+
+	s := `<html>
+<head>
+<title>Welcome to Our Website</title>
+</head>
 <body>
 <h2>Hello ` + v + ` the Tomcat server is running!</h2>
-<h1>The current server time is</h1>
-<%
-    Date date = new Date();
-    out.print("<h4 align=\"center\">" + date.toString() + "</h4>");
-%>
+<h1>The current server time is ${java.time.LocalDateTime.now()}</h1>
 </body>
 </html>`
+
 	fmt.Fprintln(file, s)
 }
